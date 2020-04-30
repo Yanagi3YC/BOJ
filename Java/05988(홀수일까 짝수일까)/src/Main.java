ecebdbf465f23aa39f028d675e912ff42813dba8
YoungChan Ryu <Yanagi3YC@gmail.com>
2020-04-30 14:07:56 +0900

    commit

---
+++ Java/05988(홀수일까 짝수일까)/src/Main.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		int num = Integer.parseInt(input.nextLine());
+		String[] inputs = new String[num];
+		String result = "";
+		for (int i = 0; i < num; i++)
+			inputs[i] = input.nextLine();
+		for (int i = 0; i < num; i++) {
+			BigInteger temp = new BigInteger(inputs[i]);
+			if (temp.remainder(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0)))
+				result += "even\n";
+			else
+				result += "odd\n";
+		}
+		System.out.print(result);
+		input.close();
+	}
+}
---
+++ Java/10809(알파벳 찾기)/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		String[] Alphabets = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+				"s", "t", "u", "v", "w", "x", "y", "z" };
+		int[] NumberOfAlphabets = new int[Alphabets.length];
+		for (int i = 0; i < NumberOfAlphabets.length; i++)
+			NumberOfAlphabets[i] = 101;
+		Scanner input = new Scanner(System.in);
+		String word = input.next();
+
+		for (int i = 0; i < Alphabets.length; i++) {
+			for (int j = 0; j < word.length(); j++) {
+				if (Alphabets[i].equals(Character.toString(word.charAt(j)))) {
+					NumberOfAlphabets[i] = j;
+					break;
+				} else
+					NumberOfAlphabets[i] = -1;
+			}
+		}
+
+		for (int i = 0; i < NumberOfAlphabets.length; i++) {
+			System.out.print(NumberOfAlphabets[i] + " ");
+		}
+
+		input.close();
+	}
+}
